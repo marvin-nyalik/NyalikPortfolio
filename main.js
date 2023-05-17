a3645ee2dd8662978834ed0a9bd91ea84f895c36
Ikonde Nekemiah Arnold <i-nad@localhost.localdomain>
2023-05-17 11:54:40 +0300

    fixed menu appearing in Desktop

--- main.js
+++ main.js
@@ -5,32 +5,32 @@ const mobileMenu = document.getElementById('mobile-menu');
 const menu_bg = document.getElementById('portfolio1');
 const { body } = document;
 
-
 function openMobileMenu() {
-    mobileMenu.style.display = 'flex';
-    body.classList.add('menu-open');
-    menu_bg.style.filter = 'blur(6px)';
-  }
-  
-function closeMobileMenu() {
-    mobileMenu.style.display = 'none';
-    menu_bg.style.filter = '';
-    body.classList.remove('menu-open');
-  }
+  mobileMenu.style.display = 'flex';
+  body.classList.add('menu-open');
+  menu_bg.style.filter = 'blur(6px)';
+}
 
-links.forEach((element) => {
-  element.addEventListener('click', closeMobileMenu());
+function closeMobileMenu() {
+  mobileMenu.style.display = 'none';
+  menu_bg.style.filter = '';
+  body.classList.remove('menu-open');
+}
+
+Array.from(links).forEach((element) => {
+  element.addEventListener('click', closeMobileMenu);
   body.classList.remove('menu-open');
 });
 
 function checkWindowSize() {
-    if (window.innerWidth > 768) {
-      mobileMenu.style.display = 'none';
-      body.classList.remove('menu-open');
-    } else {
-      mobileMenu.style.display = 'flex';
-    }
+  if (window.innerWidth > 768) {
+    mobileMenu.style.display = 'none';
+    body.classList.remove('menu-open');
+    menu_bg.style.filter = '';
+  } else {
+    mobileMenu.style.display = 'flex';
+    menu_bg.style.filter = 'blur(6px)';
   }
-  
-  window.onresize = checkWindowSize;
+}
 
+window.onresize = checkWindowSize;
