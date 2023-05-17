4653d27ffe9b6b77ed3aa8353578220dc37aaac1
Ikonde Nekemiah Arnold <i-nad@localhost.localdomain>
2023-05-17 11:44:23 +0300

    Made changes to effect menu popup

--- main.js
+++ main.js
@@ -2,23 +2,35 @@ const openMenu = document.getElementById('openBtn');
 const closeMenu = document.getElementById('closeBtn');
 const links = document.getElementsByClassName('link');
 const mobileMenu = document.getElementById('mobile-menu');
+const menu_bg = document.getElementById('portfolio1');
+const { body } = document;
 
-function closeMobileMenu() {
-  mobileMenu.style.display = 'none';
-}
 
 function openMobileMenu() {
-  mobileMenu.style.display = 'flex';
-}
+    mobileMenu.style.display = 'flex';
+    body.classList.add('menu-open');
+    menu_bg.style.filter = 'blur(6px)';
+  }
+  
+function closeMobileMenu() {
+    mobileMenu.style.display = 'none';
+    menu_bg.style.filter = '';
+    body.classList.remove('menu-open');
+  }
 
 links.forEach((element) => {
   element.addEventListener('click', closeMobileMenu());
+  body.classList.remove('menu-open');
 });
 
-openMenu.addEventListener('click', () => {
-  openMobileMenu();
-});
+function checkWindowSize() {
+    if (window.innerWidth > 768) {
+      mobileMenu.style.display = 'none';
+      body.classList.remove('menu-open');
+    } else {
+      mobileMenu.style.display = 'flex';
+    }
+  }
+  
+  window.onresize = checkWindowSize;
 
-closeMenu.addEventListener('click', () => {
-  closeMobileMenu();
-});
