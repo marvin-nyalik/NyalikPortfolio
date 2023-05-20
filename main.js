5b086b1cb4ce97c9e219f5b9c0dd4885e861fadb
MARVIN <o.silverygold@gmail.com>
2023-05-20 09:02:51 +0300

    Add initial JS file for modal popup

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 const openMenu = document.getElementById('openBtn');
-const closeMenu = document.getElementById('closeBtn');
 const links = document.getElementsByClassName('link');
 const mobileMenu = document.getElementById('mobile-menu');
-const menu_bg = document.getElementById('portfolio1');
+const menuBg = document.getElementById('portfolio1');
 const { body } = document;
 const logo = document.getElementsByClassName('logo');
 
 function openMobileMenu() {
   mobileMenu.style.display = 'flex';
   body.classList.add('menu-open');
-  menu_bg.style.filter = 'blur(6px)';
+  menuBg.style.filter = 'blur(6px)';
   logo[0].style.filter = 'blur(6px)';
 }
 
+openMenu.addEventListener('click', openMobileMenu);
+
 function closeMobileMenu() {
   mobileMenu.style.display = 'none';
-  menu_bg.style.filter = '';
+  menuBg.style.filter = '';
   logo[0].style.filter = '';
   body.classList.remove('menu-open');
 }
@@ -29,9 +30,10 @@ function checkWindowSize() {
   if (window.innerWidth > 768) {
     mobileMenu.style.display = 'none';
     body.classList.remove('menu-open');
-    menu_bg.style.filter = '';
+    menuBg.style.filter = '';
     logo[0].style.filter = '';
   }
 }
 
 window.onresize = checkWindowSize;
+
